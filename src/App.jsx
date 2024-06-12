9570268fcd0629df1d56f323501713d7b66040e0
AymanSdq <sedqiayman02@gmail.com>
2024-06-12 16:14:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='w-full h-screen flex justify-center sm:items-center'>
+        <p>Hello</p>
+      </main>
+    </>
+  )
+}
+
+export default App
